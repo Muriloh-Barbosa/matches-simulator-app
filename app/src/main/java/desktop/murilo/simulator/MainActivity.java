dee17b75197a8223a94ff7353e2024e509eda439
Muriloh-Barbosa <murilosb993@gmail.com>
2022-07-12 22:22:41 -0300

    first commit

---
+++ app/src/main/java/desktop/murilo/simulator/MainActivity.java
@@ -0,0 +1,25 @@
+package desktop.murilo.simulator;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import desktop.murilo.simulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
